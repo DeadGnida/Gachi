05a06ce8567c02fd3b7cb7c8c7a675467ba58058
DeadGnida <116429443+DeadGnida@users.noreply.github.com>
2022-10-22 11:56:19 +0300

    Add files via upload

---
+++ ConsoleApp1.0/ConsoleApp1.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("while: ");
+            var w = 10;
+            while (w++ < 99)
+                if ((w % 10) * (w / 10) % 2 == 0)
+                    Console.Write(w + " ");
+            //Вот тут всё правильно выводит
+            Console.WriteLine();
+            Console.Write("for: ");
+            var f = 10;
+            while (f++ < 99)
+                if ((f % 10) * (f / 10) % 2 == 0)
+                Console.Write(f + " ");
+            // Тут вроде тоже
+            Console.WriteLine();
+            Console.Write("   do: ");
+            var d = 10;
+            while (d++ < 99)
+                if ((d % 10) * (d / 10) % 2 == 0)
+                Console.Write(d + " ");
+            
+            //Всё выводиться верно
+                Console.ReadLine();
+            
+        }
+    }
+}
